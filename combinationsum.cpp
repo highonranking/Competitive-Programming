f5b6ce885d0e0ab2c756bf26a2b5d63d0f7b8273
abhinav dixit <abhinavdixit2306@gmail.com>
2021-06-01 12:18:29 +0530

    combinationsum1.cpp Leetcode

---
+++ combinationsum.cpp
@@ -0,0 +1,34 @@
+//problem link -> https://leetcode.com/problems/combination-sum/
+
+class Solution
+{
+public:
+    // Learnt this concept from striver(tuf)
+    void findcombination(int ind, int target, vector<int> &candidates, vector<vector<int>> &ans, vector<int> &ds)
+    {
+        if (ind == candidates.size())
+        {
+            if (target == 0)
+            {
+                ans.push_back(ds);
+            }
+            return;
+        }
+
+        if (candidates[ind] <= target)
+        {
+            ds.push_back(candidates[ind]);
+            findcombination(ind, target - candidates[ind], candidates, ans, ds);
+            ds.pop_back();
+        }
+        findcombination(ind + 1, target, candidates, ans, ds);
+    }
+
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findcombination(0, target, candidates, ans, ds);
+        return ans;
+    }
+};
